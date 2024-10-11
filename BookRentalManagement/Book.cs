7d7c27c0f5def69a6bc03ea037e870c00d76b1b2
UT01609 <UT01609@UNICOMTIC14>
2024-10-11 14:40:25 +0530

    Question_01

---
+++ BookRentalManagement/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagement
+{
+    internal class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal RentalPrice { get; set; }
+
+       
+    }
+}
