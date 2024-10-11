3021beacd15b3417e83f73e5a2f0a7fab8468769
UT01609 <UT01609@UNICOMTIC14>
2024-10-11 14:40:47 +0530

    Question_02

--- BookRentalManagement/Book.cs
+++ BookRentalManagement/Book.cs
@@ -13,6 +13,12 @@ namespace BookRentalManagement
         public string Author { get; set; }
         public decimal RentalPrice { get; set; }
 
-       
+        public Book(int bookId, string title, string author, decimal rentalPrice)
+        {
+            BookId = bookId;
+            Title = title;
+            Author = author;
+            RentalPrice = rentalPrice;
+        }
     }
 }
