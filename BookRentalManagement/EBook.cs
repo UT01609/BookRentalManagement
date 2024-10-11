d7e6a869d76b96530462ef1a31972ca25c9a9fa9
UT01609 <UT01609@UNICOMTIC14>
2024-10-11 15:04:29 +0530

    Question_06

---
+++ BookRentalManagement/EBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagement
+{
+    internal class EBook : Book
+    {
+        public double Filesize { get; set; }
+        public string Format { get; set; }
+
+        public EBook(double filesize, string format,int bookId, string title, string author, decimal rentalPrice):base(bookId, title, author, rentalPrice)
+        {
+            Filesize = filesize;
+            Format = format;
+        }
+    }
+}
