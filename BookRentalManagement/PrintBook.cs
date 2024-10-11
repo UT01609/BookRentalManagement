208b8013ddab96db599cee200ae5310e44030964
UT01609 <UT01609@UNICOMTIC14>
2024-10-11 15:07:16 +0530

    Question_07

---
+++ BookRentalManagement/PrintBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagement
+{
+    internal class PrintBook : Book
+    {
+        public int ISBN { get; set; }
+        public int NumberOfPages { get; set; }
+
+        public PrintBook(int iSBN, int numberOfPages,int bookId, string title, string author, decimal rentalPrice):base(bookId, title, author, rentalPrice)
+        {
+            ISBN = iSBN;
+            NumberOfPages = numberOfPages;
+        }
+    }
+}
